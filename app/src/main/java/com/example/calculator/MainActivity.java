20cd6340fa734a94bdeed39e68eec9159f169d3f
MIsterrio <misterio@mail.ru>
2020-03-10 04:11:22 +0700

    Доработанный калькулятор

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,12 +3,80 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private  CalculatorModel calculator;
+
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int[] numberIds = new int[] {
+                R.id.btn_0,
+                R.id.btn_1,
+                R.id.btn_2,
+                R.id.btn_3,
+                R.id.btn_4,
+                R.id.btn_5,
+                R.id.btn_6,
+                R.id.btn_7,
+                R.id.btn_8,
+                R.id.btn_9,
+
+        };
+
+        int[] actionIds = new int[] {
+                R.id.back_btn,
+                R.id.division,
+                R.id.dot_btn,
+                R.id.minus,
+                R.id.equals,
+                R.id.multiply,
+                R.id.plus,
+                R.id.clear,
+        };
+
+        text =findViewById(R.id.math_operation);
+
+        calculator = new CalculatorModel();
+//  Обработчик кнопок
+
+        View.OnClickListener nunberButtonClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculator.onNumPressed(view.getId());
+                text.setText(calculator.getText());
+
+            }
+        };
+//  Обработчик арифметических действий
+        View.OnClickListener actionButtonOnclickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculator.onActionPressed(view.getId());
+                text.setText(calculator.getText());
+            }
+        };
+
+        for (int i = 0; i < numberIds.length; i++) {
+            findViewById(numberIds[i]).setOnClickListener(nunberButtonClickListener);
+        }
+
+        for (int i = 0; i < actionIds.length; i++) {
+            findViewById(actionIds[i]).setOnClickListener(actionButtonOnclickListener);
+        }
+
+
     }
+
+
 }
+
+
+
